02a94546ce7cbc9e38f0b85849f0deae36f5f1d4
zlinker89 <ospi89@hotmail.com>
2016-02-11 16:29:36 -0500

    preparada el control de sesion

---
+++ Helloword01_02/public_html/js/sessionApp.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('miApp', []);
+// verifico si existe una sesion
+var sesion = JSON.parse(localStorage.getItem("usuario")) || null;
+if (sesion === null) {
+    location.href = "../login.html";
+} else if (sesion.tipo_usuario !== "Empleado") {
+    cerrarSession();
+}
+var url = '/api/logout/' + sesion.id;
+app.controller("pagina", function ($scope, $http) {
+    $scope.usuario = sesion;
+
+    $scope.LogOut = function () {
+        // cerramos session
+        cerrarSession();
+    }
+});
+
+function cerrarSession() {
+    localStorage.removeItem("usuario");
+    location.href = "../login.html";
+}
