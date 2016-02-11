382e7cede71deed662891d61671474eb24a20dd5
zlinker89 <ospi89@hotmail.com>
2016-02-11 11:34:33 -0500

    mejorando login

---
+++ BL/Logout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+using Entities;
+namespace BL
+{
+    public class Logout
+    {
+        private UsuarioBLL usuarioHelper = new UsuarioBLL();
+
+        public bool LogOut(long id)
+        {
+            usuarios u = usuarioHelper.GetById(id);
+            if (u.id != null)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
---
+++ Helloword01_02/Controllers/LogoutController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+namespace Helloword01_02.Controllers
+{
+    public class LogoutController : ApiController
+    {
+        private Logout l = new Logout();
+
+        /*
+        // GET api/logout
+        public usuarioDTO Get()
+        {
+            
+        }*/
+        
+        // GET api/logout/5
+        public bool Get(int id)
+        {
+            return l.LogOut(id);
+        }
+        /*
+        // POST api/logout
+        public bool Get(int id)
+        {
+            return l.LogOut(id);
+        }
+
+        // PUT api/logout/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/logout/5
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
