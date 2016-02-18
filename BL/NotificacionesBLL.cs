f05739f2f3ff6d925dad53efb469287ca91b0947
elkinpertuz6 <elccoco1995@hotmail.com>
2016-02-18 09:14:34 -0500

    codigo de envio de correos

---
+++ BL/NotificacionesBLL.cs
@@ -0,0 +1,62 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Mail;
+namespace BL
+{
+   public class NotificacionesBLL
+    {
+
+       Drummond02Entities contex = new Drummond02Entities();
+       
+       private string merror;
+       private string destino = "krloz_beto@hotmail.com";
+       private string Asunto="prueba";
+       private string mensaje="pruebaa";
+       private int contador = 1;
+       public void correos()
+       {
+           
+           var result = contex.empleado.ToList();
+           result.Equals(contador);
+           while (contador <= result.Count) 
+           {
+               var nombre=result.Select(t=> t.Nombre);
+               contador = contador + 500;
+               MailMessage Correo = new MailMessage();
+               SmtpClient protocolo = new SmtpClient();
+
+               Correo.To.Add(destino);
+               Correo.From = new MailAddress("eepertuz6@misena.edu.co", "elkin pertuz", System.Text.Encoding.UTF8);
+               Correo.Subject = Asunto;
+               Correo.SubjectEncoding = System.Text.Encoding.UTF8;
+               Correo.Body = mensaje;
+               Correo.BodyEncoding = System.Text.Encoding.UTF8;
+               Correo.IsBodyHtml = true;
+               protocolo.Credentials = new System.Net.NetworkCredential("eepertuz6@misena.edu.co", "kamazutra95");
+               protocolo.Port = 587;
+               protocolo.Host = "smtp.gmail.com";
+               protocolo.EnableSsl = true;
+               Console.Write("funciona"); 
+               try
+               {
+                   protocolo.Send(Correo);
+               }
+               catch (SmtpException error)
+               {
+                   
+                   merror = error.Message.ToString();
+               }
+            
+           }
+
+
+           
+       }
+    }
+}
---
+++ Helloword01_02/Controllers/Prueba2200Controller.cs
@@ -0,0 +1,42 @@
+﻿using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Helloword01_02.Controllers
+{
+    public class Prueba2200Controller : ApiController
+    {
+
+        NotificacionesBLL bl = new NotificacionesBLL();
+        // GET api/prueba2200
+        public void Get()
+       {
+            bl.correos();
+        }
+
+        // GET api/prueba2200/5
+        public void Get(int id)
+        {
+            bl.correos();
+        }
+
+        // POST api/prueba2200
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/prueba2200/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/prueba2200/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
