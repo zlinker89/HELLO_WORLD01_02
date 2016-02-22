02b9e661fb24c317164bee99c160e5612dd322ed
zlinker89 <ospi89@hotmail.com>
2016-02-22 16:16:31 -0500

    modificado las listas de empleados en periodos

--- Helloword01_02/Controllers/EmpleadoPeriodoController.cs
+++ Helloword01_02/Controllers/EmpleadoPeriodoController.cs
@@ -36,5 +36,62 @@ namespace Helloword01_02.Controllers
                             };
             return empleados;
         }
+
+        [Route("~/RemoveEmpleados/{inutil}")]
+        [HttpPut]
+        public empleado_seleccionadoDTO UpdateEmpleadoSeleccion(long inutil, empleado_seleccionadoDTO empleado)
+        {
+            empleados_selecionados e = db.empleados_selecionados.Where(x => x.id_empleados == empleado.id_empleados && x.id_periodos == empleado.id_periodos).FirstOrDefault();
+            e.estado = empleado.estado;
+            db.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            empleado.id = e.id;
+            empleado.id_empleados = e.id_empleados;
+            empleado.id_periodos = e.id_periodos;
+            empleado.estado = e.estado;
+            return empleado;
+        }
+
+        [Route("~/AddEmpleados/")]
+        [HttpPost]
+        public empleado_seleccionadoDTO GetEmpleadoinactivo(empleado_seleccionadoDTO empleado)
+        {
+            empleados_selecionados e = new empleados_selecionados();
+
+            e = db.empleados_selecionados.Where(x => x.id_empleados == empleado.id_empleados && x.id_periodos == empleado.id_periodos).FirstOrDefault();
+            e.estado = empleado.estado;
+            db.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            empleado.id = e.id;
+            empleado.id_empleados = e.id_empleados;
+            empleado.id_periodos = e.id_periodos;
+            empleado.estado = e.estado;
+            return empleado;
+        }
+
+        [Route("~/EmpleadosInactivos/{idperiodo}/")]
+        [HttpGet]
+        public IQueryable<empleadoDTO> GetEmpleadoinactivo(long idperiodo)
+        {
+            var empleados = from e in db.empleado
+                            join se in db.empleados_selecionados on e.id equals se.id_empleados
+                            where se.estado == 0 && se.id_periodos == idperiodo
+                            select new empleadoDTO()
+                            {
+                                id = e.id,
+                                Nombre = e.Nombre,
+                                cedula = e.cedula,
+                                id_user = e.id_user,
+                                RosterPosition = e.RosterPosition,
+                                SubArea = e.SubArea,
+                                tipo = e.tipo,
+                                Area = e.Area,
+                                CrewCd = e.CrewCd,
+                                Departament = e.Departament,
+                                Unit = e.Unit,
+                                email = e.email,
+                            };
+            return empleados;
+        }
     }
 }
